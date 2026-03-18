2edb13ab965babc29ac7f921a3cdcdca1a3b5b51
sebas-web281 <sebastian.930@hotmail.com>
2026-03-18 11:06:48 -0500

    PROYECTO PORTAOLIO #1

---
+++ assets/js/main.js
@@ -0,0 +1,168 @@
+/*=============== SHOW SIDEBAR ===============*/
+const navMenu = document.getElementById("sidebar"),
+      navToggle = document.getElementById("nav-toggle"),
+      navClose = document.getElementById("nav-close")
+
+
+/*===== SIDEBAR SHOW =====*/
+/* Validate If Constant Exists */
+if(navToggle) {
+    navToggle.addEventListener("click", () => {
+        navMenu.classList.add("show-sidebar")
+    })
+}
+
+
+/*===== SIDEBAR HIDDEN =====*/
+/* Validate If Constant Exists */
+if(navClose){
+    navClose.addEventListener("click", () => {
+        navMenu.classList.remove("show-sidebar")
+    })
+}
+
+
+/*=============== SKILLS TABS ===============*/
+const tabs = document.querySelectorAll("[data-target]"),
+tabContent = document.querySelectorAll("[data-content]")
+
+tabs.forEach(tab =>{
+    tab.addEventListener("click", ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContent.forEach(tabContent =>{
+            tabContent.classList.remove("skills__active")
+
+        })
+
+        target.classList.add("skills__active")
+
+         tabs.forEach(tab => {
+            tabs.classList.remove("skills__active")
+        })    
+
+        target.classList.add("skills__active")
+
+    })
+})
+
+
+
+/*=============== MIXITUP FILTER PORTFOLIO ===============*/
+let mixerPortfolio = mixitup(".work__container",{
+    selectors:{
+        target:".work__card"
+    },
+    animation:{
+        duration: 300
+    }
+});
+
+
+/*===== Link Active Work =====*/
+const linkwork = document.querySelectorAll(".work__item")
+
+function activeWork() {
+    linkwork.forEach(l=> l.classList.remove("active-work"))
+    this.classList.add("active-work")
+}
+
+linkwork.forEach(l=> l.addEventListener("click", activeWork))
+
+/*===== Work Popup =====*/
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("work__button")) {
+        togglePortfolioPopup();
+        portfolioItemDetails(e.target.parentElement);
+    }
+})
+function togglePortfolioPopup() {
+    document.querySelector(".portfolio__popup").classList.toggle("open");
+}
+
+document.querySelector(".portfolio__popup-close").addEventListener("click", togglePortfolioPopup)
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp__thumbnail img").src = portfolioItem.querySelector(".work__img").src;
+    document.querySelector(".portfolio__popup-subtitle span").innerHTML = portfolioItem.querySelector(".work__title").innerHTML;
+    document.querySelector(".portfolio__popup-body").innerHTML = portfolioItem.querySelector(".portafolio__item-details").innerHTML;
+    
+}
+/*=============== SERVICES MODAL ===============*/
+
+const modalViews = document.querySelectorAll(".services__modal"),
+modelBtns = document.querySelectorAll(".services__button"),
+modalCloses = document.querySelectorAll(".services__modal-close")
+
+let modal = function(modalClick){
+    modalViews[modalClick].classList.add("active-modal")
+}
+
+modelBtns.forEach((modelBtn, i) => {
+    modelBtn.addEventListener("click", () =>{
+        modal(i)
+    })
+})
+modalCloses.forEach((modalCloses) => {
+    modalCloses.addEventListener("click", () =>{
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove("active-modal")
+        })
+
+    })
+})
+/*=============== SWIPER TESTIMONIAL ===============*/
+
+
+/*=============== INPUT ANIMATION ===============*/
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+    let parent = this.parentNode;
+    parent.classList.add("focus");
+}
+
+function blurFunc() {
+    let parent = this.parentNode;
+    if(this.value == "") {
+        parent.classList.remove("focus");
+    }
+}
+inputs.forEach((input) => {
+    input.addEventListener("focus", focusFunc);
+    input.addEventListener("blur", blurFunc)
+})
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+// Obtener todas las secciones que tengan un id
+const sections = document.querySelectorAll("section[id]");
+
+// Agregar evento al hacer scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() 
+
+{
+  // Obtener la posición actual del scroll
+  let scrollY = window.pageYOffset;
+
+  // Recorrer cada sección
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50,
+    sectionId = current.getAttribute("id");
+
+    // Verificar si el scroll está dentro de la sección
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) 
+        {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.add("active-link");
+    } 
+    else 
+        {
+      document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
+    }
+  })
+}
+
+
+/*=============== SHOW SCROLL UP ===============*/
